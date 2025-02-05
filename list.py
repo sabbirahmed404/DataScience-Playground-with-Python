20a3b0d79e107b3e0bc639dd1f9fe562d23fdffc
Sabbirahmed404 <msa29.cse@gmail.com>
2025-02-05 10:36:07 +0600

    practiced list items

--- list.py
+++ list.py
@@ -1,1 +1,12 @@
-print("Hello World")
+numlist = [3, 9, 4, 2, 4, 1, 5, 6, 10, 8, 7, 9]
+
+res = []
+
+for i in numlist:
+    if i not in res:
+        res.append(i)
+
+print(res)
+
+
+# print(sorted(list(set(numlist))))
