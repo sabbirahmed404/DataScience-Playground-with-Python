7868b57f751bf9490aef7291306d04dcb19826b2
Sabbirahmed404 <msa29.cse@gmail.com>
2025-03-02 22:48:28 +0600

    added some file

--- leetcode/Can-Place-Flowers.py
+++ leetcode/Can-Place-Flowers.py
@@ -1,15 +1,16 @@
-def solution(flowerbed: list[int], n: int) -> bool:
+def solution( flowerbed: list[int], n: int) -> bool:
     count = 0
     for x in range(len(flowerbed)):
         if flowerbed[x] == 0:
-            prev_empty = (x == 0) or (flowerbed[x-1] == 0)
-            next_empty = (x == len(flowerbed)-1) or (flowerbed[x+1] == 0)
-            if prev_empty and next_empty:
+            prev = flowerbed[0] == 0 or flowerbed[x-1] == 0
+            nex = flowebed[x-1] == 0 or flowerbed[x+1] == 0
+            if prev and nex:
                 flowerbed[x] = 1
                 count += 1
                 if count >= n:
                     return True
     
+        
     return count >= n            
 
 
@@ -20,3 +21,4 @@ if __name__ == "__main__":
     print(flowerbed)
     result = solution(flowerbed, n)
     print(result)
+
