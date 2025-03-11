1be3540ec0f392a4662937d5e57ab45f49495937
Sabbirahmed404 <msa29.cse@gmail.com>
2025-03-11 17:28:55 +0600

    added manual DFS

--- leetcode/DFS_2.py
+++ leetcode/DFS_2.py
@@ -1,3 +1,23 @@
+def dfs_rec(adj, visited, s):
+    visited[s] = True
+
+    print(s, end=" ")
+
+    for i in adj[s]:
+        if not visited[i]:
+            dfs_rec(adj, visited, i)
+
+
+def dfs(adj, s):
+    visited = [False] * len(adj)
+    dfs_rec(adj, visited, s)
+
+
+def add_edge(adj, t, s):
+    adj[t].append(s)
+    adj[s].append(t)
+
+
 if __name__ == "__main__":
     V =5
     adj = [[] for _ in range(V)]
@@ -5,4 +25,9 @@ if __name__ == "__main__":
     edges = [[1,2], [1,0], [2,0], [2,3], [2,4]]
 
     for e in edges:
-        print(e)
+        add_edge(adj, e[0], e[1])
+
+    source = 1
+    print("DFS from source: ", source)
+    dfs(adj, source)
+    print((adj))
